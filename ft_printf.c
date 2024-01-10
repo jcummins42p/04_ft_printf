4e9d515bfeca7b80c503fe608235ee6e503b6e74
John Cummins <jcummins@c3r3s5.42prague.com>
2024-01-10 12:01:14 +0100

    "Readding printf"

---
+++ ft_printf.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jcummins <jcummins@student.42prague.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/11 15:09:09 by jcummins          #+#    #+#             */
+<<<<<<< HEAD
+/*   Updated: 2024/01/10 11:51:13 by jcummins         ###   ########.fr       */
+=======
+/*   Updated: 2023/12/15 20:21:45 by jcummins         ###   ########.fr       */
+>>>>>>> refs/remotes/origin/main
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	format(va_list arg, char c)
+{
+	int				len;
+	unsigned int	u_arg;
+
+	len = 0;
+	if (c == 'd' || c == 'i')
+		len += print_int(va_arg(arg, int));
+	if (c == 'u')
+	{
+		u_arg = (unsigned int)va_arg(arg, int);
+		len += print_usi(u_arg);
+	}
+	if (c == 'c')
+		len += print_char(va_arg(arg, int));
+	if (c == 's')
+		len += print_str(va_arg(arg, char *));
+	if (c == 'x')
+		len += print_hex(va_arg(arg, int), 0);
+	if (c == 'X')
+		len += print_hex(va_arg(arg, int), 1);
+	if (c == 'p')
+		len += print_ptr(va_arg(arg, void *));
+	else if (c == '%')
+		len += print_char('%');
+	return (len);
+}
+
+int	ft_printf(const char *str, ...)
+{
+	va_list	argptr;
+	int		i;
+	int		len;
+
+	i = 0;
+	len = 0;
+	va_start(argptr, str);
+	while (str[i])
+	{
+		if (str[i] == '%')
+		{
+			i++;
+			len += format(argptr, str[i]);
+			i++;
+		}
+		else
+		{
+			write(1, &str[i], 1);
+			len++;
+			i++;
+		}
+	}
+	va_end(argptr);
+	return (len);
+}
